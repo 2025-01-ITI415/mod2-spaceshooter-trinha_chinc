e6e170f3c9119913e917360800ecebef4b831611
Caitlin <195965395+CC2041@users.noreply.github.com>
2025-03-27 17:13:41 -0400

    fixed the things i broke
    yea

--- Assets/__Scripts/GameOverManager.cs
+++ Assets/__Scripts/GameOverManager.cs
@@ -22,5 +22,6 @@ public class GameOverManager : MonoBehaviour
 
         // Update the score text on the game over screen
         scoreText.text = "Final Score: " + score.ToString();
+        Time.timeScale = 0;
     }
 }
