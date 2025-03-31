a047b6e3828eb51eb023d39e832db354e20d56c4
Caitlin <195965395+CC2041@users.noreply.github.com>
2025-03-30 21:43:02 -0400

    Added Sound control
    sound menu

---
+++ Assets/SoundSettings.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+
+        // Add listeners to update volume on slider change
+        musicSlider.onValueChanged.AddListener(delegate { SetMusicVolume(); });
+        SFXSlider.onValueChanged.AddListener(delegate { SetSFXVolume(); });
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+}
--- Assets/__Scripts/MainMenu.cs
+++ Assets/__Scripts/MainMenu.cs
@@ -12,5 +12,6 @@ public void QuitGame(){
     Debug.Log("Quit");
     Application.Quit();
 }
+
 }
     
